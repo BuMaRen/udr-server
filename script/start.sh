e299cc32a0ef783ba2ecfbcce924b90d1f00b4c9
BuMaRen <hj.961110@qq.com>
2023-11-26 19:32:50 +0800

    ++tcp/ftp/sftp server

---
+++ script/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 /tmp/ftp-start.py
+python3 /tmp/tcp-server.py
--- script/tcp-server.py
+++ script/tcp-server.py
@@ -1,28 +1,30 @@
 import os
-from socketserver import TCPServer, BaseServer, BaseRequestHandler
+import logging
+from socketserver import TCPServer, BaseRequestHandler
 from threading import Thread, Semaphore
+logging.basicConfig(filename='./udr-server.log', encoding='utf-8', level=logging.DEBUG)
 
 class MyTCPHandler(BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print(self.data)
+        logging.debug(self.data)
         
 def run_tcp_server(port, sema:Semaphore):
     with TCPServer(('localhost', port), MyTCPHandler) as server:
-        print(f'监听本地全部ip的{port}号端口')
+        logging.error(f'监听本地全部ip的{port}号端口')
         server.serve_forever()
     sema.release(1)
 
-
-
 if __name__ == "__main__":
+    logging.debug('main start')
     ports = os.environ.get('TCP_PORT', None) or '10500'
     port_list = ports.split(',')
     sema = Semaphore(0)
     counter = 0
     for port in port_list:
-        t = Thread(target=run_tcp_server, args=[port, sema])
+        t = Thread(target=run_tcp_server, args=[int(port), sema])
         t.start()
+        logging.error("1 thread started")
         counter+=1
     while counter > 0:
         sema.acquire()
